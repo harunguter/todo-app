49f2c90a9593f27c84c90f83cf737722310954b9
Harun Guter <harunguter@gmail.com>
2023-08-15 11:36:11 +0300

    minor fixes

--- server/src/index.js
+++ server/src/index.js
@@ -19,6 +19,6 @@ app.use(cors({ origin: "*" }));
 
 app.use(loggerMiddleware.logger());
 
-app.use("/api/todo", todoRoute);
+app.use("todo", todoRoute);
 
 app.listen(port, () => logger.info(`API started, Port: ${port}`));

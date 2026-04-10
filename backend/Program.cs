0d57589af0366491124b35aec9816db91d66dfbb
Harun Guter <harunguter@gmail.com>
2026-04-10 16:21:42 +0300

    updated packages

--- backend/Program.cs
+++ backend/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddDbContext<TodoDbContext>(options =>
 builder.Services.AddCors();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
@@ -36,7 +36,7 @@ app.MapGet(route, async (TodoDbContext context) =>
 {
     var todos = await context.Todos.ToListAsync();
 
-    Log.Information("Fetched todos: {@Todos}", todos);
+    Log.Information("Fetch todos: {@Todos}", todos);
 
     return Results.Ok(new ApiResponse(data: todos));
 });
@@ -49,7 +49,7 @@ app.MapGet($"{route}/{{id}}", async (string id, TodoDbContext context) =>
     var findTodo = await context.Todos.FindAsync(id);
     if (findTodo is not Todo) throw new Exception("Todo not found.");
 
-    Log.Information("Finded todo: {@FindTodo}", findTodo);
+    Log.Information("Find todo: {@FindTodo}", findTodo);
 
     return Results.Ok(new ApiResponse(data: findTodo));
 });
@@ -67,7 +67,7 @@ app.MapPost(route, async (Todo todo, TodoDbContext context) =>
     var addedTodo = context.Todos.Add(todo);
     await context.SaveChangesAsync();
 
-    Log.Information("Added todo: {@AddedTodo}", addedTodo.Entity);
+    Log.Information("Add todo: {@AddedTodo}", addedTodo.Entity);
 
     return Results.Ok(new ApiResponse(data: addedTodo.Entity));
 });
@@ -86,7 +86,7 @@ app.MapPut($"{route}/{{id}}", async (string id, Todo todo, TodoDbContext context
 
     var updatedTodo = context.Todos.Update(findTodo);
 
-    Log.Information("Updated todo: {@UpdatedTodo}", updatedTodo.Entity);
+    Log.Information("Update todo: {@UpdatedTodo}", updatedTodo.Entity);
 
     await context.SaveChangesAsync();
 
@@ -130,7 +130,6 @@ app.Use(async (context, next) =>
 
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
-app.UseSwagger();
-app.UseSwaggerUI();
+app.MapOpenApi();
 
 app.Run();
